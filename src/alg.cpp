3a0b520bb2e8d48e3df1d96f55739560cafdeef4
Serotoninchik <160415794+Serotoninchik@users.noreply.github.com>
2024-03-26 02:05:27 +0300

    b179

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,78 +1,63 @@
-// Copyright 2021 NNTU-CS
+// Copyr 2021 NNTU-CS
+
 
 int countPairs1(int *arr, int len, int value) {
   return 0;
-#include <unistd.h>
-#include <iostream>
-#include <algorithm>
-
-int countPairs3(int* arr, int len, int value);
-int countPairs1(int* arr, int len, int value) {
-    int in = 0;
-    while (in < 10000000) {
-        in++;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        count++;
+      }
     }
-    int count = 0;
-    int r = len - 1;
-    while (arr[r] >= value)
-        r--;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if ((arr[i] + arr[j]) == value) {
-                count++;
-            }
-        }
-    }
-    if (count)
-        return count;
-    return 0;
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
-
-int countPairs2(int* arr, int len, int value) {
-    sleep(2);
-    int ooo = 0;
-    int r = len - 1;
-    int g = value - arr[0];
-    while (arr[r] >= value)
-        r--;
-    return countPairs3(arr, len, value);
+  int count = 0;
+  int t = len - 1;
+  while (arr[t] > value) {
+    t--;
+  }
+  for (int i = 0; i < len; i++) {
+    for (int j = t; j > i; j--) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        count++;
+      } else if (arr[i] + arr[j] < value) {
+        break;
+      }
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
   return 0;
-
-int countPairs3(int* arr, int len, int value) {
-    int count = 0;
-    int l;
-    int r, mid, SecVal;
-    for (int i = 0; i < len; ++i) {
-        r = len - 1;
-        l = i + 1;
-        SecVal = value - arr[i];
-        while (l <= r) {
-            mid = (l + r) / 2;
-            if (arr[mid] == SecVal) {
-                count++;
-                int p = middle - 1;
-                while ((p > i) && (arr[p] == arr[mid])) {
-                    count++;
-                    p--;
-                }
-                int p2 = mid + 1;
-                while ((i < p2) && (arr[p2] == arr[mid])) {
-                    count++;
-                    p2++;
-                }
-                break;
-            } else if (arr[mid] > SecVal) {
-                r = mid - 1;
-            } else {
-                lft = mid + 1;
-            }
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    int r = len;
+    int l = i;
+    while (l < r - 1) {
+      int mid = (l + r) / 2;
+      if (arr[i] + arr[mid] == value) {
+        count++;
+        int t = mid + 1;
+        while ((arr[i] + arr[t] == value) && (t < r)) {
+          count++;
+          t++;
         }
+        t = mid - 1;
+        while ((arr[i] + arr[t] == value) && (t > l)) {
+          count++;
+          t--;
+        }
+        break;
+      } else if (arr[i] + arr[mid] > value) {
+        r = mid;
+      } else {
+        l = mid;
+      }
     }
-    if (count)
-        return count;
-    return 0;
+  }
+  return count;
 }
