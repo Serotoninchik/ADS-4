b6b8b3e6ced3381ac9845426ef311f266ed88332
Serotoninchik <160415794+Serotoninchik@users.noreply.github.com>
2024-03-26 01:46:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,55 +1,76 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
+  return 0;
+int countPairs1(int * arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
+  for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        count += 1;
-      }
-    }
-  }
-  return count;
-}
-
-int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = len - 1; j > i; j--) {
-      if (arr[i] + arr[j] == value) {
-        count += 1;
-      }
-    }
-  }
-  return count;
-}
-
-int countPairs3(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int s = i, f = len;
-    while (1 < f - s) {
-      int mid = (s + f) / 2;
-      if (arr[i] + arr[mid] == value) {
         count++;
-        int r = mid + 1;
-        while (arr[i] + arr[r] == value && r < f) {
-          count++;
-          r++;
-        }
-        r = mid - 1;
-        while (arr[i] + arr[r] == value && r > s) {
-          count++;
-          r--;
-        }
-        break;
-      }
-      if (arr[i] + arr[mid] > value) {
-        f = mid;
-      } else {
-        s = mid;
       }
     }
   }
+
+  return count;
+}
+int countPairs2(int *arr, int len, int value) {
+  return 0;
+int countPairs2(int * arr, int len, int value) {
+  int count = 0;
+  int r = len - 1;
+
+  while (arr[r] > value) {
+    r--;
+  }
+
+  for (int l = 0; l < r; l++) {
+    for (int r = r; r > l; r--) {
+      if (arr[l] + arr[r] == value) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+int countPairs3(int *arr, int len, int value) {
+  return 0;
+int countPairs3(int * arr, int len, int value) {
+  int count = 0;
+
+  for (int i = 0; i < len; i++) {
+    int l = i + 1;
+    int r = len - 1;
+    int target = value - arr[i];
+
+    while (l <= r) {
+      int mid = (l + r) / 2;
+
+      if (arr[mid] == target) {
+        count++;
+        int tempMid = mid - 1;
+
+        while (tempMid > i && arr[tempMid] == target) {
+          count++;
+          tempMid--;
+        }
+
+        tempMid = mid + 1;
+
+        while (tempMid < len - 1 && arr[tempMid] == target) {
+          count++;
+          tempMid++;
+        }
+
+        break;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+  }
+
   return count;
 }
