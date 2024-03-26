4871431b281c93a8ff08b0cdc6de79e1a1766d08
Serotoninchik <160415794+Serotoninchik@users.noreply.github.com>
2024-03-26 03:45:58 +0300

    b179

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,5 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int *arr, int len, int value) {
-  return 0;
 int binSearch(int* arr, int len, int value) {
     int count = 0;
     int lt = 0;
@@ -30,8 +28,6 @@ int binSearch(int* arr, int len, int value) {
     }
     return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
 int countPairs1(int* arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
@@ -43,8 +39,6 @@ int countPairs1(int* arr, int len, int value) {
     }
     return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
 int countPairs2(int* arr, int len, int value) {
     int count = 0;
     int lt = 0;
